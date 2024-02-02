94e04d6e73ba0de483132fbf3523b8610fb86629
Karina <rikes.kar@gmail.com>
2024-02-02 21:24:31 +0100

    wsgi error

--- locallibrary/wsgi.py
+++ locallibrary/wsgi.py
@@ -13,7 +13,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'locallibrary.settings')
 
-application = get_wsgi_application()
+#application = get_wsgi_application()
 
 
 # Para despliegue en Vercel
